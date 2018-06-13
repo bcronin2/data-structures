250ee77c27e44a7a42df1f6ea6317f569612e5ae
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-06-13 14:27:56 -0700

    implement stack and queue using pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,17 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.front = 0;
+  this.back = 0;
 };
 
-
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.back++] = value;
+};
+Queue.prototype.dequeue = function() {
+  if (this.size() > 0) {
+    return this.storage[this.front++];
+  }
+};
+Queue.prototype.size = function() {
+  return this.back - this.front;
+};
