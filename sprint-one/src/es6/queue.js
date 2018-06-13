8f56f6fe38fdc51597214c4effaa4a2de112406c
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-06-13 14:35:43 -0700

    clean up es6 implementations

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,22 +1,22 @@
 class Queue {
   constructor() {
-  	this.storage = {};
-  	this.front = 0;
-  	this.back = 0;
+    this.storage = {};
+    this.front = 0;
+    this.back = 0;
   }
 
   enqueue(value) {
-  	this.storage[this.back++] = value;
+    this.storage[this.back++] = value;
   }
 
   dequeue() {
-  	if (this.size() > 0) {
-  		return this.storage[this.front++];
-  	}
+    if (this.size() > 0) {
+      return this.storage[this.front++];
+    }
   }
 
   size() {
-  	return this.back - this.front;
+    return this.back - this.front;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,21 +1,21 @@
 class Stack {
   constructor() {
-  	this.storage = {};
-  	this.top = 0;
+    this.storage = {};
+    this.top = 0;
   }
 
   push(value) {
-  	this.storage[this.top++] = value;
+    this.storage[this.top++] = value;
   }
 
   pop() {
-  	if (this.size() > 0) {
-  		return this.storage[--this.top];
-  	}
+    if (this.size() > 0) {
+      return this.storage[--this.top];
+    }
   }
 
   size() {
-  	return this.top;
+    return this.top;
   }
 
 }
