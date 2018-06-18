922fda0245767f94d2c2eef372750cc7ce6bec2f
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-06-18 13:53:04 -0700

    refine spacing in toString method

--- advanced-content/src/balancingSearchTree.js
+++ advanced-content/src/balancingSearchTree.js
@@ -1,13 +1,12 @@
 class BalancingSearchTree {
-  
   constructor(value, parent) {
     this.value = value;
     this.parent = parent || null;
-    this.left = {height: 0};
-    this.right = {height: 0};
+    this.left = { height: 0 };
+    this.right = { height: 0 };
     this.height = 1;
   }
-  
+
   // O(log n)
   insert(value) {
     if (value < this.value) {
@@ -26,7 +25,7 @@ class BalancingSearchTree {
     this.height = Math.max(this.left.height, this.right.height) + 1;
     this._balanceIfNecessary();
   }
-  
+
   // O(log n)
   contains(value) {
     if (value === this.value) {
@@ -37,18 +36,18 @@ class BalancingSearchTree {
     }
     if (value > this.value && this.right.height) {
       return this.right.contains(value);
-    }   
+    }
     return false;
   }
-  
+
   // O(n)
   depthFirstLog(cb) {
     cb(this.value);
     if (this.left.height) {
-      this.left.depthFirstLog(cb); 
-    } 
+      this.left.depthFirstLog(cb);
+    }
     if (this.right.height) {
-      this.right.depthFirstLog(cb); 
+      this.right.depthFirstLog(cb);
     }
   }
 
@@ -61,22 +60,32 @@ class BalancingSearchTree {
       str += spacesForRow(rowIndex + 1, rows.length);
       var parentIndex = 0;
 
-      for (var childIndex = 0; childIndex < rows[rowIndex].length; childIndex++) {
+      for (
+        var childIndex = 0;
+        childIndex < rows[rowIndex].length;
+        childIndex++
+      ) {
         var childNode = rows[rowIndex][childIndex];
         var parentNode = rows[rowIndex - 1][parentIndex];
+        var nodeLength = childNode.value.toString().length;
 
-        while (childNode.parent !== parentNode && parentIndex < rows[rowIndex - 1].length) {
-          str += spacesForRow(rowIndex - 1, rows.length);
+        while (
+          childNode.parent !== parentNode &&
+          parentIndex < rows[rowIndex - 1].length
+        ) {
+          str += spacesForRow(rowIndex - 1, rows.length, nodeLength);
           parentNode = rows[rowIndex - 1][++parentIndex];
         }
 
         if (childNode.value < parentNode.value) {
           str += childNode.value;
         } else {
-          str += (spacesForRow(rowIndex, rows.length) + childNode.value + spacesForRow(rowIndex, rows.length));
+          str +=
+            spacesForRow(rowIndex, rows.length, nodeLength) +
+            childNode.value +
+            spacesForRow(rowIndex, rows.length);
           parentIndex++;
         }
-
       }
 
       str += '\n\n';
@@ -85,14 +94,13 @@ class BalancingSearchTree {
     return str;
   }
 
-
   // HELPER FUNCTIONS
 
   _balanceIfNecessary() {
     if (this.left.height - this.right.height > 1) {
       this._balance('left', 'right');
     } else if (this.right.height - this.left.height > 1) {
-      this._balance('right', 'left');      
+      this._balance('right', 'left');
     }
 
     return this;
@@ -105,10 +113,12 @@ class BalancingSearchTree {
       if (newRoot[side1].height) {
         newRoot[side1].parent = newRoot.parent;
       }
-      newRoot.parent.height = Math.max(newRoot.parent[side1].height, newRoot.parent[side2].height) + 1;
+      newRoot.parent.height =
+        Math.max(newRoot.parent[side1].height, newRoot.parent[side2].height) +
+        1;
 
       var newChild = new BalancingSearchTree(this.value, this);
-      newChild[side2] = this[side2];    
+      newChild[side2] = this[side2];
 
       this.value = newRoot.value;
       this[side2].parent = newChild;
@@ -148,18 +158,19 @@ class BalancingSearchTree {
       if (nextNode.right.height) {
         nodes.push(nextNode.right);
       }
-      if (nodes.length && 
-          (nextNode.value > nodes[0].value || nextNode === nodes[0].parent)) {
+      if (
+        nodes.length &&
+        (nextNode.value > nodes[0].value || nextNode === nodes[0].parent)
+      ) {
         rows.push([]);
-      } 
+      }
     }
     return rows;
   }
-  
 }
 
-var spacesForRow = function(index, total) {
-  return nSpaces(Math.pow(2, 2 + total - index) - 3);
+var spacesForRow = function(index, total, offset) {
+  return nSpaces(Math.pow(2, 2 + total - index) - (offset ? offset : 3));
 };
 
 var nSpaces = function(n) {
